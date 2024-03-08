82a3ab0f37d44ec4ea2d39c31e225bd4e401ca7b
XerAnime <160598198+XerAnime@users.noreply.github.com>
2024-03-08 20:07:07 +0800

    Fix

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -15,6 +15,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </head>
       <body className={`${roboto.className} relative`}>
         <NavBar />
         {children}
